3d56a555d7e1604d5e4a0e631f2b609fbb1a70ed
Kaan Kuscu <me@kaanksc.com>
2024-06-28 00:03:34 +0300

    add godoc comments

--- kache.go
+++ kache.go
@@ -26,11 +26,13 @@ func New[K comparable, V any]() *Kache[K, V] {
 	return k
 }
 
+// WithMaxRecordNum sets the maximum number of records to keep in the cache.
 func (k *Kache[K, V]) WithMaxRecordNum(num int) *Kache[K, V] {
 	k.maxRecordNum = num
 	return k
 }
 
+// Set sets a value in the cache.
 func (k *Kache[K, V]) Set(key K, v V) {
 	defer k.lock()()
 	k.store[key] = valueWithTTL[V]{
@@ -38,6 +40,7 @@ func (k *Kache[K, V]) Set(key K, v V) {
 	}
 }
 
+// SetWithTTL sets a value in the cache with a TTL. If the TTL is 0, the value will not expire.
 func (k *Kache[K, V]) SetWithTTL(key K, v V, ttl time.Duration) {
 	defer k.lock()()
 	var ttlVal time.Time
@@ -50,12 +53,42 @@ func (k *Kache[K, V]) SetWithTTL(key K, v V, ttl time.Duration) {
 	}
 }
 
+// Get gets a value from the cache. Returns false in second value if the key does not exist.
 func (k *Kache[K, V]) Get(key K) (V, bool) {
 	defer k.lock()()
 	item, ok := k.store[key]
 	return item.value, ok
 }
 
+// Delete deletes a value from the cache.
+func (k *Kache[K, V]) Delete(key K) {
+	defer k.lock()()
+	delete(k.store, key)
+}
+
+// Flush deletes all values from the cache.
+func (k *Kache[K, V]) Flush() {
+	defer k.lock()()
+	clear(k.store)
+}
+
+// Keys returns all keys in the cache.
+func (k *Kache[K, V]) Keys() []K {
+	defer k.lock()()
+	keys := make([]K, 0, len(k.store))
+	for k := range k.store {
+		keys = append(keys, k)
+	}
+	return keys
+}
+
+// Count returns the number of values in the cache.
+func (k *Kache[K, V]) Count() int {
+	defer k.lock()()
+	return len(k.store)
+}
+
+// Poll deletes expired values from the cache with the given poll interval. If context is cancelled, the polling stops.
 func (k *Kache[K, V]) Poll(ctx context.Context, pollInterval time.Duration) {
 	ticker := time.NewTicker(pollInterval)
 	defer ticker.Stop()
@@ -78,30 +111,6 @@ func (k *Kache[K, V]) Poll(ctx context.Context, pollInterval time.Duration) {
 			}
 		}
 	}
-}
-
-func (k *Kache[K, V]) Delete(key K) {
-	defer k.lock()()
-	delete(k.store, key)
-}
-
-func (k *Kache[K, V]) Flush() {
-	defer k.lock()()
-	clear(k.store)
-}
-
-func (k *Kache[K, V]) Keys() []K {
-	defer k.lock()()
-	keys := make([]K, 0, len(k.store))
-	for k := range k.store {
-		keys = append(keys, k)
-	}
-	return keys
-}
-
-func (k *Kache[K, V]) Count() int {
-	defer k.lock()()
-	return len(k.store)
 }
 
 func (k *Kache[K, V]) lock() func() {
