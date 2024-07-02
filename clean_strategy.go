4f0add357134338876e365ab92b5d79da2f43f3d
Kaan Kuscu <me@kaanksc.com>
2024-07-03 00:57:15 +0300

    implement new replacement strategies

--- clean_strategy.go
+++ clean_strategy.go
@@ -1,53 +1,16 @@
 package kache
 
-type cleanStrategy uint
+type ReplacementStrategy uint
 
 const (
-	cleanStrategyNone cleanStrategy = iota
-	cleanStrategyLRU
-	cleanStrategyFIFO
+	ReplacementStrategyNone ReplacementStrategy = iota
+	ReplacementStrategyLRU                      // Least Recently Used
+	ReplacementStrategyMRU                      // Most Recently Used
+	ReplacementStrategyFIFO                     // First In First Out
+	ReplacementStrategyLIFO                     // Last In First Out
+	ReplacementStrategyLFU                      // Least Frequently Used
+	ReplacementStrategyMFU                      // Most Frequently Used
 )
-
-// WithCleanStrategyLRU sets the clean strategy to LRU and Deletes the specified number of records, if possible, when the maximum number of records is reached
-//
-// LRU: Least Recently Used
-//
-// maxRecordCount: maximum number of records
-//
-// recordsToClean: number of records to clean
-//
-// example: WithCleanStrategyLRU(10000, 1000), if record number reaches 10000, it will delete 1000 records
-func (k *Kache[K, V]) WithCleanStrategyLRU(maxRecordCount int, recordsToClean int) *Kache[K, V] {
-	k.cleanStrategy = cleanStrategyLRU
-	k.maxRecordNum = maxRecordCount
-	k.cleanNum = recordsToClean
-	return k
-}
-
-// WithCleanStrategyFIFO sets the clean strategy to FIFO and Deletes the specified number of records, if possible, when the maximum number of records is reached
-//
-// FIFO: First In First Out
-//
-// maxRecordCount: maximum number of records
-//
-// recordsToClean: number of records to clean
-//
-// example: WithCleanStrategyFIFO(10000, 1000), if record number reaches 10000, it will delete 1000 records
-func (k *Kache[K, V]) WithCleanStrategyFIFO(maxRecordCount int, recordsToClean int) *Kache[K, V] {
-	k.cleanStrategy = cleanStrategyFIFO
-	k.maxRecordNum = maxRecordCount
-	k.cleanNum = recordsToClean
-	return k
-}
-
-// WithCleanStrategyNone sets the clean strategy to none, so no records will be deleted,
-// except for those who use ttl
-func (k *Kache[K, V]) WithCleanStrategyNone() *Kache[K, V] {
-	k.cleanStrategy = cleanStrategyNone
-	k.maxRecordNum = -1
-	k.cleanNum = -1
-	return k
-}
 
 func (k *Kache[K, V]) clean() {
 	currentElem := k.elems.Front()
--- kache.go
+++ kache.go
@@ -2,7 +2,7 @@ package kache
 
 import (
 	"container/list"
-	"context"
+	"fmt"
 	"sync"
 	"time"
 )
@@ -10,25 +10,47 @@ import (
 type valueWithTTL[K comparable, V any] struct {
 	key        K
 	value      V
+	hitCount   uint
 	expireTime time.Time
 }
 
 type Kache[K comparable, V any] struct {
-	elems         *list.List // front of list == greater risk of deletion <---------list---------> back of list == less risk of deletion
-	store         map[K]*list.Element
-	mut           *sync.Mutex
-	maxRecordNum  int
-	cleanNum      int
-	cleanStrategy cleanStrategy
+	elems               *list.List // front of list == greater risk of deletion <---------list---------> back of list == less risk of deletion
+	store               map[K]*list.Element
+	mut                 *sync.Mutex
+	maxRecordThreshold  int
+	cleanNum            int
+	replacementStrategy ReplacementStrategy
+	pollInterval        time.Duration
+	pollCancel          chan struct{}
 }
 
-func New[K comparable, V any]() *Kache[K, V] {
-	k := &Kache[K, V]{
-		elems:        list.New(),
-		store:        make(map[K]*list.Element),
-		mut:          new(sync.Mutex),
-		maxRecordNum: -1,
+type Config struct {
+	ReplacementStrategy ReplacementStrategy // default: ReplacementStrategyNone
+	MaxRecordTreshold   int                 // This parameter is used to control the maximum number of records in the cache. If the number of records exceeds this threshold, records will be deleted according to the replacement strategy.
+	CleanNum            int                 // This parameter is used to control the number of records to be deleted.
+	PollInterval        time.Duration       // This parameter is used to control the polling interval. If value is 0, uses default = 1 second.
+}
+
+func New[K comparable, V any](cfg Config) *Kache[K, V] {
+	pollInterval := time.Second
+	if cfg.PollInterval > 0 {
+		pollInterval = cfg.PollInterval
 	}
+
+	k := &Kache[K, V]{
+		elems:               list.New(),
+		store:               make(map[K]*list.Element),
+		mut:                 new(sync.Mutex),
+		maxRecordThreshold:  cfg.MaxRecordTreshold,
+		cleanNum:            cfg.CleanNum,
+		replacementStrategy: cfg.ReplacementStrategy,
+		pollInterval:        pollInterval,
+		pollCancel:          make(chan struct{}),
+	}
+
+	go k.poll()
+
 	return k
 }
 
@@ -45,14 +67,28 @@ func (k *Kache[K, V]) SetWithTTL(key K, v V, ttl time.Duration) {
 // Get gets a value from the cache. Returns false in second value if the key does not exist.
 func (k *Kache[K, V]) Get(key K) (V, bool) {
 	defer k.lock()()
+
 	item, ok := k.store[key]
 	if !ok {
 		return *new(V), false
 	}
-	if k.cleanStrategy == cleanStrategyLRU {
-		k.elems.MoveToBack(item)
+
+	value := item.Value.(*valueWithTTL[K, V])
+
+	if k.replacementStrategy == ReplacementStrategyMFU || k.replacementStrategy == ReplacementStrategyLFU {
+		value.hitCount++
 	}
-	return item.Value.(*valueWithTTL[K, V]).value, true
+
+	switch k.replacementStrategy {
+	case ReplacementStrategyLRU:
+		k.elems.MoveToBack(item)
+	case ReplacementStrategyMRU:
+		k.elems.MoveToFront(item)
+	case ReplacementStrategyMFU, ReplacementStrategyLFU:
+		k.moveByHits(item)
+	}
+
+	return value.value, true
 }
 
 // Delete deletes a value from the cache.
@@ -73,8 +109,8 @@ func (k *Kache[K, V]) Flush() {
 func (k *Kache[K, V]) Keys() []K {
 	defer k.lock()()
 	keys := make([]K, 0, len(k.store))
-	for k := range k.store {
-		keys = append(keys, k)
+	for e := k.elems.Front(); e != nil; e = e.Next() {
+		keys = append(keys, e.Value.(*valueWithTTL[K, V]).key)
 	}
 	return keys
 }
@@ -85,16 +121,23 @@ func (k *Kache[K, V]) Count() int {
 	return k.elems.Len()
 }
 
+func (k *Kache[K, V]) Close() {
+	close(k.pollCancel)
+}
+
 // Poll deletes expired values from the cache with the given poll interval. If context is cancelled, the polling stops.
-func (k *Kache[K, V]) Poll(ctx context.Context, pollInterval time.Duration) {
-	ticker := time.NewTicker(pollInterval)
+func (k *Kache[K, V]) poll() {
+	fmt.Println("polling", k.pollInterval)
+	ticker := time.NewTicker(k.pollInterval)
 	defer ticker.Stop()
 
 	for {
 		select {
-		case <-ctx.Done():
+		case <-k.pollCancel:
+			fmt.Println("poll canceled")
 			return
 		case <-ticker.C:
+			fmt.Println("polling")
 			k.mut.Lock()
 			now := time.Now()
 			for key := range k.store {
@@ -109,6 +152,8 @@ func (k *Kache[K, V]) Poll(ctx context.Context, pollInterval time.Duration) {
 			k.mut.Unlock()
 		}
 	}
+
+	fmt.Println("poll stopped")
 }
 
 func (k *Kache[K, V]) lock() func() {
@@ -118,18 +163,62 @@ func (k *Kache[K, V]) lock() func() {
 
 func (k *Kache[K, V]) set(key K, v V, ttl time.Duration) {
 	defer k.lock()()
-	if k.maxRecordNum > 0 && len(k.store) >= k.maxRecordNum {
+	if k.maxRecordThreshold > 0 && k.cleanNum > 0 && k.replacementStrategy > ReplacementStrategyNone && len(k.store) >= k.maxRecordThreshold {
 		k.clean()
 	}
+
 	value := &valueWithTTL[K, V]{
 		key:        key,
 		value:      v,
 		expireTime: time.Now().Add(ttl),
 	}
+
+	// if exists
 	if oldElem, ok := k.store[key]; ok {
 		oldElem.Value = value
-		k.elems.MoveToBack(oldElem)
+		switch k.replacementStrategy {
+		case ReplacementStrategyLRU:
+			k.elems.MoveToBack(oldElem)
+		case ReplacementStrategyMRU:
+			k.elems.MoveToFront(oldElem)
+		}
 		return
 	}
-	k.store[key] = k.elems.PushBack(value)
+
+	// if not exists
+	switch k.replacementStrategy {
+	case ReplacementStrategyFIFO, ReplacementStrategyLRU, ReplacementStrategyLFU, ReplacementStrategyMFU:
+		k.store[key] = k.elems.PushBack(value)
+	case ReplacementStrategyLIFO, ReplacementStrategyMRU:
+		k.store[key] = k.elems.PushFront(value)
+	}
+}
+
+func (k *Kache[K, V]) moveByHits(elem *list.Element) {
+	prev := elem.Prev()
+	next := elem.Next()
+	switch k.replacementStrategy {
+	case ReplacementStrategyLFU:
+		if prev != nil && prev.Value.(*valueWithTTL[K, V]).hitCount > elem.Value.(*valueWithTTL[K, V]).hitCount {
+			k.elems.MoveBefore(elem, prev)
+			k.moveByHits(elem)
+			return
+		}
+
+		if next != nil && next.Value.(*valueWithTTL[K, V]).hitCount < elem.Value.(*valueWithTTL[K, V]).hitCount {
+			k.elems.MoveAfter(elem, next)
+			k.moveByHits(elem)
+		}
+	case ReplacementStrategyMFU:
+		if prev != nil && prev.Value.(*valueWithTTL[K, V]).hitCount < elem.Value.(*valueWithTTL[K, V]).hitCount {
+			k.elems.MoveBefore(elem, prev)
+			k.moveByHits(elem)
+			return
+		}
+
+		if next != nil && next.Value.(*valueWithTTL[K, V]).hitCount > elem.Value.(*valueWithTTL[K, V]).hitCount {
+			k.elems.MoveAfter(elem, next)
+			k.moveByHits(elem)
+		}
+	}
 }
